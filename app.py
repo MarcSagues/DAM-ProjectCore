5a9991b0a219f16ba87ce45f98f012b4e7e6cc0f
Jordi Mateo Fornés (UdL) <61190134+JordiMateoUdL@users.noreply.github.com>
2020-05-03 13:24:30 +0200

    Sprint III. Corrections and suggestions

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ application.add_route("/users/show/{username}", user_resources.ResourceGetUserPr
 application.add_route("/favours", event_resources.ResourceGetEvents())
 application.add_route("/favours/update/{id}", event_resources.UpdateFavour())
 application.add_route("/favours/delete/{id}", event_resources.DeleteFavour())
-application.add_route("/favours/post/{id}", event_resources.ResourcePostFavour())
+application.add_route("/favours/add/", event_resources.ResourcePostFavour())
 application.add_route("/account/update_profile", user_resources.UpdateUser())
 
 application.add_sink(handle_404, "")
